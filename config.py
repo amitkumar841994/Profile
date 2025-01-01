a835a731e152f68f2a8c054223ca017a13b91146
amitkumar841994 <amitkumar841994@gmail.com>
2025-01-01 11:39:05 +0530

    template added

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-# from pymongo.mongo_client import MongoClient
-
-# # MongoDB connection URI
-# uri = "mongodb://amitkumar841994:bAyyuwJouF5lSctK@cluster0.cl04m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# # Create MongoDB client
-# client = MongoClient(uri)
-
-# # Connect to the database
-# try:
-#     client.admin.command("ping")
-#     print("Pinged your deployment. Successfully connected to MongoDB!")
-# except Exception as e:
-#     print("Error connecting to MongoDB:", e)
-
-# # Define a database instance
-# db = client["profile"]  # Replace "mydatabase" with your database name
-
 
 
 from pymongo.mongo_client import MongoClient
