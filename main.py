7f5120522dfc014b8725f416a6f2c8ec3b7e417d
Amitkumar Pal <apal@sapat.com>
2025-01-01 18:29:44 +0530

    html

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from authentication.routes import router as auth_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
 # Include routers from each app
 app.include_router(auth_routes, prefix="/app1", tags=["App1"])
 
