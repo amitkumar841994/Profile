18af75b778fb23bd59845bcd53cd5667637f4c90
amitkumar841994 <amitkumar841994@gmail.com>
2025-01-06 09:31:13 +0530

    add session and working on Login page

--- authentication/views.py
+++ authentication/views.py
@@ -5,40 +5,42 @@ from config import db
 from utils.password import pwd_context
 
 
-
-
 class NewUserRegistration:
     def __init__(self):
         self.router = APIRouter()
-        self.router.add_api_route("/items", self.register, methods=["POST"])
-        # self.router.add_api_route("/items", self.create_item, methods=["POST"])
+        self.router.add_api_route("/register", self.register, methods=["POST"])
+        self.router.add_api_route("/login", self.user_login, methods=["POST"])
 
     async def register(self, new_user:NewUser):
-            if not new_user.user_id:
-                new_user.user_id = uuid.uuid4()
+        if not new_user.user_id:
+            new_user.user_id = uuid.uuid4()
 
-            try:
-                result = db.User.find_one({ '$or': [{'email':new_user.email}, {'mobile':new_user.mobile}]})
-                print("User>>>>>>>", result)
-                if result:
-                    if new_user.email== result['email'] :
-                        return {  "message": f"{new_user.email} already registered"}
-                    if new_user.mobile== result['mobile'] :
-                        return {  "message": f"{new_user.mobile} already registered"}   
-                else :   
-                    new_user.password =pwd_context.hash(new_user.password)  
-                    result = db.User.insert_one(new_user.model_dump())
-                    return {
+        try:
+            result = db.User.find_one({ '$or': [{'email':new_user.email}, {'mobile':new_user.mobile}]})
+            print("User>>>>>>>", result)
+            if result:
+                if new_user.email== result['email'] :
+                    return {  "message": f"{new_user.email} already registered"}
+                if new_user.mobile== result['mobile'] :
+                    return {  "message": f"{new_user.mobile} already registered"}   
+            else :   
+                new_user.password =pwd_context.hash(new_user.password)  
+                result = db.User.insert_one(new_user.model_dump())
+                return {
                     "message": "registered successfully",
-                    "data": new_user.model_dump()
+                    "data": new_user.model_dump(),
+                    "status_code":201
                     }
-            except Exception as e:
-                 print("Error inserting",e)
-        
+        except Exception as e:
             return {
-                "message": "Item created",
-                "data": new_user.model_dump()
-            }
+                    "message": f"{str(e)}",
+                    "status_code":500
+                    }
+
     async def user_login(self,login:UserLogin):
-         pass
-         
+        try:
+            user_details = db.User.find_one(
+                {"$or": [{"email": login.email}, {"mobile": login.mobile}]}
+            )
+        except Exception as e:
+            return {"message": f"{str(e)}", "status_code": 500}
--- utils/password.py
+++ utils/password.py
@@ -1,3 +1,46 @@
 from passlib.context import CryptContext
 
+from fastapi import FastAPI,Depends,HTTPException,status
+from jose import JWTError,jwt
+from datetime import datetime,timedelta
+from pydantic import BaseModel
+
+
+# Secret keys and algorithms
+SECRET_KEY = "vakjshdfaksjfh74356837lakjhdgflas457687sfgagask;jg"
+REFRESH_SECRET_KEY = "b;lskdjftgq3849tuldsk;jgta654654awetfgaswzjnehtgf"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+# Helper functions
+def create_token(data: dict, expires_delta: timedelta, secret_key: str):
+    """Create a JWT token."""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
+
+
+def create_access_token(data: dict):
+    """Create an access token."""
+    return create_token(
+        data, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES), SECRET_KEY
+    )
+
+
+def create_refresh_token(data: dict):
+    """Create a refresh token."""
+    return create_token(
+        data, timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS), REFRESH_SECRET_KEY
+    )
+ 
